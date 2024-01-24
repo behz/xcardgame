e72347c7f6b64c6b4ca9bc455df2e0772dcd28c5
Behzad Ghaffarian <behzad.ghaffarian@gmail.com>
2024-01-24 23:39:04 +0100

    add @vercel/speed-insights/next

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           {children}
         </main>
         <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   );
